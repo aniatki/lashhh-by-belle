6b1524e43fcd6acc5cee29b796df0289315dadad
Hasan Atki <djamblers@gmail.com>
2022-08-26 16:18:53 +0100

    Add functionality to date modal for booking

---
+++ assets/scripts/modal.js
@@ -0,0 +1,6 @@
+const modal = document.querySelector(".date-time-modal")
+const button = document.querySelector(".primary-btn")
+
+button.addEventListener("click", () => {
+    modal.showModal()
+})
