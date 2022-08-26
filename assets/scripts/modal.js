9ebb694ac0445915d3fafc02bc58758e16c7fbd6
Hasan Atki <djamblers@gmail.com>
2022-08-26 19:50:29 +0100

    Add functionality for votes

--- assets/scripts/modal.js
+++ assets/scripts/modal.js
@@ -1,5 +1,14 @@
+// Modal stuff
 const modal = document.querySelector(".date-time-modal")
+const form = modal.querySelector("form")
 const button = document.querySelector(".primary-btn")
+const input = modal.querySelector("input")
+
+const LOCAL_STORAGE_KEY = `bookLashAppointment${new Date().getSeconds()}`
+
+form.addEventListener("submit", (e) => {
+    JSON.stringify(localStorage.setItem(LOCAL_STORAGE_KEY, input.value))
+})
 
 button.addEventListener("click", () => {
     modal.showModal()
