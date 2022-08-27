ae0becc5db635d380ec73e5e83611f2bf263e2e7
Hasan Atki <djamblers@gmail.com>
2022-08-27 17:34:51 +0100

    Style small buttons and fix menu appearance

---
+++ assets/scripts/user-dashboard.js
@@ -0,0 +1,44 @@
+const main = document.querySelector(".dashboard-main")
+
+let itemsFromLocalStorage = []
+getLocalStorageItems()
+function getLocalStorageItems() {
+    for (let i = 0; i < localStorage.length; i++) {
+        let key = localStorage.key(i)
+        let item = localStorage.getItem(key)
+        itemsFromLocalStorage.push(item)
+    }
+}
+
+itemsFromLocalStorage.forEach((dateFrom) => {
+    const dateObj = Date.parse(Date(dateFrom))
+    const bookModel = {
+        appDate: Date(dateFrom).slice(0, 10),
+        appTime: Date(dateFrom).slice(16, 24),
+        // appNumber,
+        // packageName,
+        // packageDescription,
+        // packageLength,
+        // packagePrice,
+    }
+    const appItem = `
+    <div class="app-item">
+        <div class="appointment">
+            <span class="details app-date">${bookModel.appDate}</span>
+            <span class="details app-time">${bookModel.appTime}</span>
+            <span class="details app-length"></span>
+            <span class="details app-number accent"></span>
+        </div>
+        <p class="package">
+        </p>
+    
+        <div class="app-buttons">
+            <span class="app-price"></span>
+            <button class="app-book">Book again</button>
+            <button class="app-cancel">Cancel</button>
+        </div>
+    </div>
+    `
+
+    main.innerHTML += appItem
+})
